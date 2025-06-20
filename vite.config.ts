b37ed908cc189e2a070ac71d6c48a64334d208eb
之铭 <1198072251@qq.com>
2025-06-20 16:44:40 +0800

    init
    删除无用文件

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    server: {
+        host: '0.0.0.0',
+        port: 3000,
+        open: '/',
+    },
+    resolve: {
+        // 路径别名配置
+        alias: {
+            '@': path.resolve(import.meta.dirname, 'src'),
+        },
+    },
+    plugins: [react()],
 })
