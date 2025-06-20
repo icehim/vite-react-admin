58d76f3fa74daafeb5935f9563d7f4f397623f24
之铭 <1198072251@qq.com>
2025-06-20 18:37:32 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Button } from 'antd'
+
 function App() {
-  return <div className="App">Vite-React-App</div>
+  return (
+    <div className="App">
+      <h1>Vite-React-App</h1>
+      <Button type="primary">Button</Button>
+    </div>
+  )
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from '@/App.tsx'
 import '@/common/styles/frame.scss'
+import { ConfigProvider } from 'antd'
+import '@ant-design/v5-patch-for-react-19'
+import zhCN from 'antd/locale/zh_CN'
 
-createRoot(document.getElementById('root')!).render(
+const container = document.getElementById('root')
+if (!container) throw new Error('root 容器未找到')
+
+const root = createRoot(container)
+
+root.render(
   <StrictMode>
-    <App />
+    <ConfigProvider locale={zhCN}>
+      <App />
+    </ConfigProvider>
   </StrictMode>
 )
